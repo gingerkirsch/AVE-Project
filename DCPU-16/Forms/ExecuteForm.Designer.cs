336e241d27565d4f39fea7cae777ce68d64f1d4c
Alexandra Sankova <sandra.sankova@gmail.com>
2014-05-10 11:37:20 +0100

    Add Output/Dump form (GUI)

---
+++ DCPU-16/Forms/ExecuteForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DCPU_16.Forms
+{
+    partial class ExecuteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Dump = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Output);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.Dump);
+            this.splitContainer1.Size = new System.Drawing.Size(488, 379);
+            this.splitContainer1.SplitterDistance = 160;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.SystemColors.MenuText;
+            this.Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Output.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Output.ForeColor = System.Drawing.Color.White;
+            this.Output.Location = new System.Drawing.Point(0, 0);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(488, 160);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // Dump
+            // 
+            this.Dump.BackColor = System.Drawing.Color.White;
+            this.Dump.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Dump.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Dump.Location = new System.Drawing.Point(0, 0);
+            this.Dump.Name = "Dump";
+            this.Dump.Size = new System.Drawing.Size(488, 215);
+            this.Dump.TabIndex = 0;
+            this.Dump.Text = "";
+            // 
+            // ExecuteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 379);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "ExecuteForm";
+            this.Text = "DCPU-16";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Dump;
+    }
+}
---
+++ DCPU-16/Forms/ExecuteForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCPU_16.Forms
+{
+    public partial class ExecuteForm : Form
+    {
+        public ExecuteForm()
+        {
+            InitializeComponent();
+            Dump.ReadOnly = true;
+            Output.ReadOnly = true;
+        }
+
+        public void UpdateDamp(string damp)
+        {
+            Dump.Text = damp;
+        }
+
+        public void UpdateOutput(string output)
+        {
+            Output.Text = output;
+        }
+
+        /*private void ExecuteForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1.HasDump = false;
+        }*/
+    }
+}
